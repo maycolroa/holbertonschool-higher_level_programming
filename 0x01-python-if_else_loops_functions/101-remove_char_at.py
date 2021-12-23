87d04cd3a351d76da871eebafa28835aa083ea6d
maycolroa <lehallo@hotmail.com>
2021-12-22 23:46:11 -0500

    0x01. Python - if/else, loops, functions  0.09 22/12/21

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 def remove_char_at(string, i):
-if i >= 0:
-string_1 = string[:i]
-string_2 = string[i + 1:]
-string = string_1 + string_2
-return string
+    if (i >= 0):
+        return string[:i] + string[i + 1:]
+    else:
+        return string
