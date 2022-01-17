5d09c70f2946ae6184f3972dcb887abd19b4808a
maycolroa <lehallo@hotmail.com>
2022-01-17 18:35:44 -0500

    0x05. Python - Exceptions  0.03  17/01/22

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def safe_print_division(a, b):
     try:
-        splitting_operation = (a / b)
-    except ZeroDivisionError:
+        splitting_operation = a / b
+    except (TypeError, ZeroDivisionError):
         splitting_operation = None
     finally:
         print("Inside splitting_operation: {}".format(splitting_operation))
