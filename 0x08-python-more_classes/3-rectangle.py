1f5341a191bfce7ee386bb7196eb91b57c871e3e
maycolroa <lehallo@hotmail.com>
2022-02-19 21:12:21 -0500

    0x08. Python - More Classes and Objects  0.02 19/02/22

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -7,6 +7,7 @@ class Rectangle:
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
+
     def __str__(self):
         if self.__height == 0 or self.__width == 0:
             return ''
@@ -20,6 +21,7 @@ class Rectangle:
     @property
     def width(self):
         return self.__width
+
     @width.setter
     def width(self, value):
         if not isinstance(value, int):
@@ -27,11 +29,14 @@ class Rectangle:
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
+
     @property
     def height(self):
         return self.__height
+
     @height.setter
     def height(self, value):
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -40,6 +45,7 @@ class Rectangle:
 
     def area(self):
         return self.__width * self.__height
+
     def perimeter(self):
         if self.__height == 0 or self.__width == 0:
             return 0
