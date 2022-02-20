ac283c3539e79e9c32b3cc2937cd8c0fdf9877b7
maycolroa <lehallo@hotmail.com>
2022-02-19 21:13:59 -0500

    0x08. Python - More Classes and Objects  0.02 19/02/22

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -12,6 +12,7 @@ class Rectangle:
         if self.__height == 0 or self.__width == 0:
             return ''
         rec_str = ''
+
         for k in range(self.__height):
             for j in range(self.__width):
                 rec_str += '#'
@@ -24,6 +25,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
+
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
