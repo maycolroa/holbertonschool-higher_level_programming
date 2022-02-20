d623863fb3f15641ff0e9179433160cd8223077d
maycolroa <lehallo@hotmail.com>
2022-02-19 21:21:00 -0500

    0x08. Python - More Classes and Objects  0.02 19/02/22

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -1,59 +1,48 @@
 #!/usr/bin/python3
-"""class."""
+'''class Rectangle'''
 
 
 class Rectangle:
-
     def __init__(self, width=0, height=0):
-
         self.width = width
         self.height = height
 
-    def __str__(self):
-
-        if self.__height == 0 or self.__width == 0:
-            return ''
-        rec_str = ''
-        for i in range(self.__height):
-            for j in range(self.__width):
-                rec_str += '#'
-            rec_str += '\n'
-        return rec_str[:-1]
-
     @property
     def width(self):
-
         return self.__width
+
+    @property
+    def height(self):
+        return self.__height
 
     @width.setter
     def width(self, value):
-
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
-    @property
-    def height(self):
-
-        return self.__height
-
     @height.setter
     def height(self, value):
-
-        if not isinstance(value, int):
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
-
-        return self.__width * self.__height
+        return self.width * self.__height
 
     def perimeter(self):
-
         if self.__height == 0 or self.__width == 0:
             return 0
-        return 2 * (self.__width + self.__height)
+        per = (2 * self.__height) + (2 * self.__width)
+        return per
+
+    def __str__(self):
+        if self.__width == 0 or self.__height == 0:
+            return ""
+        for j in range(self.__height - 1):
+            print("#" * self.__width)
+        return ("#" * self.__width)
