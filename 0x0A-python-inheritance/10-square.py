caa1141dbb200eff094bbc1469930a38374745a3
maycolroa <lehallo@hotmail.com>
2022-03-23 20:55:35 -0500

    0x0A. Python - Inheritance  23/03/22 0.09

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""define class"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """class Square"""
+    def __init__(self, size):
+        """initial """
+        self.integer_validator("size", size)
+        self._size = size
+        super().__init__(size, size)
--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""class BaseGeometry"""
+"""define class"""
 
 
 class BaseGeometry:
