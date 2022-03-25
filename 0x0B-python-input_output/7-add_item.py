62eeb234ffed7656e8e0c5842d126214884a235e
maycolroa <lehallo@hotmail.com>
2022-03-25 01:58:01 -0500

    0x0B-python-input_output  24/03/22 0.07

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,16 +1,31 @@
 #!/usr/bin/python3
-"""the function """
+"""the function Add items """
+
+from os import path
 from sys import argv
 
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
 
-save_from_json = __import__('5-save_to_json_file').save_to_json_file
-load_from_json = __import__('6-load_from_json_file').load_from_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
 
-filename = "add_item.json"
 
-try:
-    content = load_from_json_file(filename)
-except:
-    content = []
+def add_items():
+    """add items"""
 
-save_from_json(content + argv[1:], filename)
+    counter = []
+
+    """Check"""
+    if path.isfile("add_item.json"):
+        counter = load_from_json_file("add_item.json")
+
+    if len(argv) > 1:
+
+        for j in range(1, len(argv)):
+            counter.append(argv[j])
+
+    """ Write file """
+    
+    save_to_json_file(counter, "add_item.json")
+
+if __name__ == "__main__":
+    add_items()
