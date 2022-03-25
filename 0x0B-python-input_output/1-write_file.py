4150051e1c1a6a8c7cd62eaf57b9ebdad6f05ac0
maycolroa <lehallo@hotmail.com>
2022-03-25 00:15:50 -0500

    0x0B-python-input_output  24/03/22 0.01

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""the function write """
+
+
+def write_file(filename="", text=""):
+    """writes a string (UTF8)"""
+    with open(filename, "w", encoding="UTF-8") as f:
+        return(f.write(text))
