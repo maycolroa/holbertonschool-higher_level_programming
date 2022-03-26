4edb48be38382ad6b90b3a772f1c5e41ea3811d0
maycolroa <lehallo@hotmail.com>
2022-03-25 19:58:51 -0500

    0x0B-python-input_output  24/03/22 0.100

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -3,17 +3,18 @@
 
 
 def append_after(filename="", search_string="", new_string=""):
-	"""search"""
-	counter = []
-	with open(filename, "r", encoding="utf-8") as f:
-		counter = f.readlines()
-		intercom = 0
+    """search"""
 
-		while intercom < len(counter):
-			if search_string in counter[intercom]:
-				counter[intercom:intercom + 1] = [counter[intercom], new_string]
-				intercom += 1
-			intercom += 1
+    counter = []
+    with open(filename, "r", encoding="utf-8") as f:
+        counter = f.readlines()
+        intercom = 0
 
-	with open(filename, "w", encoding="utf-8") as file:
-		file.writelines(counter)
+        while intercom < len(counter):
+            if search_string in counter[intercom]:
+                counter[intercom:intercom + 1] = [counte[intercom], new_string]
+                intercom += 1
+            intercom += 1
+
+    with open(filename, "w", encoding="utf-8") as file:
+        file.writelines(counter)
