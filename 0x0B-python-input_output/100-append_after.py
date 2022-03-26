2111372d84cb714483c082685c643ef611c9eb3d
maycolroa <lehallo@hotmail.com>
2022-03-25 19:51:39 -0500

    0x0B-python-input_output  24/03/22 0.100

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""append_after"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+	"""search"""
+	counter = []
+	with open(filename, "r", encoding="utf-8") as f:
+		counter = f.readlines()
+		intercom = 0
+
+		while intercom < len(counter):
+			if search_string in counter[intercom]:
+				counter[intercom:intercom + 1] = [counter[intercom], new_string]
+				intercom += 1
+			intercom += 1
+
+	with open(filename, "w", encoding="utf-8") as file:
+		file.writelines(counter)
