b3e8ab4859be07e5061ecca26ede9a6c728d63b0
maycolroa <lehallo@hotmail.com>
2022-04-04 23:09:59 -0500

    0x12. JavaScript - Warm up  04/04/22 0.03

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const argv1 = process.argv;
-if (argv1[2] === undefined) {
+if (process.argv[2] === undefined) {
     console.log('No argument');
   } else {
     console.log(process.argv[2]);
