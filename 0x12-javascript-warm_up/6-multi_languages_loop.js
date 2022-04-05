1465ae366429190c258a729937a21a2b8139cb9b
maycolroa <lehallo@hotmail.com>
2022-04-05 00:29:13 -0500

    0x12. JavaScript - Warm up  04/04/22 0.06

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const newlist = ['C is fun','Python is cool', 'JavaScript is amazing'];
+const newlist = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let j = 0; newlist[j]; j++) {
-    console.log(newlist[j]);
+  console.log(newlist[j]);
 }
