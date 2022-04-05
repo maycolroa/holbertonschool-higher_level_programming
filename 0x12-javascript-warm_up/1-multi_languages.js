9fe0bfa8b0a9c73fd7e4ed70ad532b5c829d6648
maycolroa <lehallo@hotmail.com>
2022-04-04 21:56:09 -0500

    0x12. JavaScript - Warm up  04/04/22 0.01

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-const i = "C is fun";
-const j = "Python is cool";
-const k = "JavaScript is amazing";
+const i = 'C is fun';
+const j = 'Python is cool';
+const k = 'JavaScript is amazing';
 console.log(i); console.log(j); console.log(k);
