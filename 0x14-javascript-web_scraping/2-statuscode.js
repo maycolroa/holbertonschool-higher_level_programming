60b7486712a45765232dc07774264f28ed9a3119
maycolroa <lehallo@hotmail.com>
2022-04-12 08:16:44 -0500

    0x14-javascript-web_scraping  12/04/22 0.02

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+//Write a script that display the status code of a GET request.
+const request = require('request');
+const test = process.argv[2];
+request(test, function (err, response) {
+  if (err) throw err;
+  console.log('code:', response && response.statusCode);
+});
