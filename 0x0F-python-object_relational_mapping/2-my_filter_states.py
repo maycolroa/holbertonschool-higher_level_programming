62f9846cb1d8051f833dd50b936a2dd6a39d2c1d
maycolroa <lehallo@hotmail.com>
2022-05-10 10:54:39 -0500

    0x0F. Python - Object-relational mapping  0.02 10/05/22

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Write a script that takes in an argument and displays all values in the states table"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    name = argv[4]
+    cur = db.cursor()
+    cur.execute("""SELECT * FROM states ORDER BY id ASC""".format(name))
+    query = cur.fetchall()
+    for j in query:
+        if j[1] == name:
+            print(j)
+    cur.close()
+    db.close()
