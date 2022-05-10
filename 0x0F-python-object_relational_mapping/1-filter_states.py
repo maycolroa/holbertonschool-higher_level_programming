83b540113d315659832ed56792d5304614663263
maycolroa <lehallo@hotmail.com>
2022-05-10 10:30:45 -0500

    0x0F. Python - Object-relational mapping  0.01 10/05/22

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Write a script that lists all states from the database"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=argv[1],
+            passwd=argv[2],
+            db=argv[3])
+    cur = db.cursor()
+    cur.execute("""
+                SELECT *
+                FROM states
+                WHERE name LIKE BINARY 'N%'
+                ORDER BY id ASC
+                """
+    for j in cur.fetchall():
+        print(j)
+    cur.close()
+    db.close()
