e7b3cbc07b0ea04a520c9cd4f2d05ce12f262f18
maycolroa <lehallo@hotmail.com>
2022-05-13 18:06:12 -0500

    0x0F. Python - Object-relational mapping  0.06 13/05/22

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""Write a script that takes in the name of a state as an argument and lists all cities"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+    db = MySQLdb.connect(user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT cities.name FROM cities\
+                 INNER JOIN states ON cities.state_id = states.id\
+                 WHERE states.name = %s\
+                 ORDER BY cities.id", (argv[4], ))
+    query = cur.fetchall()
+    counter = 0
+    for j in query:
+        if counter != 0:
+            print(", ", end="")
+        print("%s" % j, end="")
+        counter += 1
+    print("")
+    cur.close()
+    db.close()
