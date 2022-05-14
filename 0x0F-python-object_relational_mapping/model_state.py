64281ac4f9b1706ef26cd4833f2471f8b04f9e5c
maycolroa <lehallo@hotmail.com>
2022-05-13 19:07:30 -0500

    0x0F. Python - Object-relational mapping  0.07 13/05/22

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -8,6 +8,6 @@ Base = declarative_base()
 
 class State(Base):
     """class that inherits from Base"""
-    __tablename__ = 'states'
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True, nullable=False)
-    name = Column(String(128), nullable=False
+    name = Column(String(100), nullable=False
