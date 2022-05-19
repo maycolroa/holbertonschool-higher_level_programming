6ef3a6269fd74353cbd69032eb225a1225a80207
maycolroa <lehallo@hotmail.com>
2022-05-18 19:49:13 -0500

    0x10. Python - Network #0 0.06 18/05/22

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -3,7 +3,10 @@
 
 
 def find_peak(list_of_integers):
-
-    if list_of_integers:
-        list_of_integers.sort()
-        return list_of_integers[-1]
+    """function that finds a peak in a list of unsorted integers."""
+    lint = list_of_integers
+    if lint:
+        lint.sort()
+        return lint[-1]
+    else:
+        return None
