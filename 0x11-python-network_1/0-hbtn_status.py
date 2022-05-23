2e17eaf96be1b67648e77f9ed10d327a9e22d520
maycolroa <lehallo@hotmail.com>
2022-05-23 16:47:38 -0500

    0x11. Python - Network #1 0.00 23/05/22

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 """Write a Python script that fetches"""
-
-
-if __name__ == "__main__":
+def main():
     import urllib.request
     with urllib.request.urlopen("https://intranet.hbtn.io/status") as response:
-        html = response.read()
-        print("html response:")
-        print("\t- type: {}".format(type(html)))
-        print("\t- content: {}".format(html))
-        print("\t- utf8 content: {}".format(html.decode("utf-8")))
+        body = response.read()
+        print("Body response:")
+        print("\t- type: {}".format(type(body)))
+        print("\t- content: {}".format(body))
+        print("\t- utf8 content: {}".format(body.decode("UTF-8")))
+if __name__ == "__main__":
+    main()
